57c5e240135a1fa0b1a51d84fdcc93fbc6b84c98
seph <barca014@gmail.com>
2016-07-26 18:43:05 +0800

    revert their last changes

--- Bubble.js
+++ Bubble.js
@@ -48,7 +48,7 @@ export default class Bubble extends React.Component {
   }
 
   renderText(text = '', position) {
-    if (this.props.renderCustomText && this.props.renderCustomText(this.props) !==false) {
+    if (this.props.renderCustomText) {
       return this.props.renderCustomText(this.props);
     }
 
@@ -96,11 +96,8 @@ export default class Bubble extends React.Component {
 
   render() {
     const flexStyle = {};
-    const realLength = function(str) {
-      return str.replace(/[^\x00-\xff]/g, "**").length; // [^\x00-\xff] - Matching non double byte character
-    };
     if (this.props.text) {
-      if (realLength(this.props.text) > 40) {
+      if (this.props.text.length > 40) {
         flexStyle.flex = 1;
       }
     }
